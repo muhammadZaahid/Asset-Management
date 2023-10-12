55fc163389c346b88fdff3c44f04c74f6b7334ec
Zaahid <muhammadzaahid24@gmail.com>
2023-10-12 12:01:38 +0700

    Add technician initial project

---
+++ technician-service/src/main/java/com/app/technician/TechnicianApplication.java
@@ -0,0 +1,13 @@
+package com.app.technician;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicianApplication.class, args);
+	}
+
+}
---
+++ technician-service/src/test/java/com/app/technician/TechnicianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.technician;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
