e7e3ad4952e0cfdf47a76e7e510ad83318ddfcfd
Zaahid <muhammadzaahid24@gmail.com>
2023-10-15 13:42:43 +0700

    Update Technician Repository

--- technician-service/src/main/java/com/app/technician/repository/TechnicianRepository.java
+++ technician-service/src/main/java/com/app/technician/repository/TechnicianRepository.java
@@ -1,9 +1,15 @@
 package com.app.technician.repository;
 
+import com.app.asset.model.Asset;
 import com.app.technician.model.Technician;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TechnicianRepository extends JpaRepository<Technician, String> {
+
+    @Query(value = " SELECT * FROM technician t " +
+            " WHERE t.id = ?1", nativeQuery = true)
+    Technician findTechnicianById(String id);
 }
