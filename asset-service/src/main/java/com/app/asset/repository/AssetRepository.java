6fd714feb9b6c99c5cf1ee1c748a128972f49a03
Zaahid <muhammadzaahid24@gmail.com>
2023-10-15 12:54:54 +0700

    Update Repository

--- asset-service/src/main/java/com/app/asset/repository/AssetRepository.java
+++ asset-service/src/main/java/com/app/asset/repository/AssetRepository.java
@@ -1,9 +1,16 @@
 package com.app.asset.repository;
 
+import com.app.asset.dto.AssetRes;
 import com.app.asset.model.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AssetRepository extends JpaRepository<Asset, String> {
+
+    @Query(value = " SELECT * FROM asset a " +
+            " WHERE a.id = ?1", nativeQuery = true)
+    Asset findAssetById(String id);
+
 }
