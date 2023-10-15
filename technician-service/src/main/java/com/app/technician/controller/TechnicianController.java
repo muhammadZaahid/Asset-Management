7bc4ccea3d078c18a9f11568647b085a2b0deca2
Zaahid <muhammadzaahid24@gmail.com>
2023-10-15 13:42:26 +0700

    Update Technician Controller

--- technician-service/src/main/java/com/app/technician/controller/TechnicianController.java
+++ technician-service/src/main/java/com/app/technician/controller/TechnicianController.java
@@ -1,6 +1,9 @@
 package com.app.technician.controller;
 
-import com.app.technician.model.Technician;
+import com.app.technician.dto.BaseRes;
+import com.app.technician.dto.TechnicianInsertReq;
+import com.app.technician.dto.TechnicianRes;
+import com.app.technician.dto.TechnicianUpdateReq;
 import com.app.technician.service.TechnicianService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,20 +24,20 @@ public class TechnicianController {
     }
 
     @PostMapping
-    public ResponseEntity<Technician> createTechnician(@RequestBody Technician technician) {
-        Technician createdTechnician = technicianService.createTechnician(technician);
-        return new ResponseEntity<>(createdTechnician, HttpStatus.CREATED);
+    public ResponseEntity<BaseRes> createTechnician(@RequestBody TechnicianInsertReq request) {
+        BaseRes response = technicianService.createTechnician(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Technician>> getAllTechnicians() {
-        List<Technician> technicians = technicianService.getAllTechnicians();
+    public ResponseEntity<List<TechnicianRes>> getAllTechnicians() {
+        List<TechnicianRes> technicians = technicianService.getAllTechnicians();
         return new ResponseEntity<>(technicians, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Technician> getTechnicianById(@PathVariable String id) {
-        Technician technician = technicianService.getTechnicianById(id);
+    public ResponseEntity<TechnicianRes> getTechnicianById(@PathVariable String id) {
+        TechnicianRes technician = technicianService.getTechnicianById(id);
         if (technician != null) {
             return new ResponseEntity<>(technician, HttpStatus.OK);
         } else {
@@ -43,22 +46,22 @@ public class TechnicianController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Technician> updateTechnician(@PathVariable String id, @RequestBody Technician updatedTechnician) {
-        Technician updated = technicianService.updateTechnician(id, updatedTechnician);
-        if (updated != null) {
-            return new ResponseEntity<>(updated, HttpStatus.OK);
+    public ResponseEntity<BaseRes> updateTechnician(@PathVariable String id, @RequestBody TechnicianUpdateReq request) {
+        BaseRes response = technicianService.updateTechnician(id, request);
+        if (response != null) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteTechnician(@PathVariable String  id) {
-        boolean deleted = technicianService.deleteTechnician(id);
-        if (deleted) {
-            return new ResponseEntity<>("Technician deleted successfully", HttpStatus.OK);
+    public ResponseEntity<BaseRes> deleteTechnician(@PathVariable String id) {
+        BaseRes response = technicianService.deleteTechnician(id);
+        if (response != null) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>("Technician not found", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
