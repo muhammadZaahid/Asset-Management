2f593e9bbde8032a4af73fb8a5d86efe2d4a642a
Zaahid <muhammadzaahid24@gmail.com>
2023-10-15 13:41:52 +0700

    Add DTO

---
+++ technician-service/src/main/java/com/app/technician/dto/TechnicianUpdateReq.java
@@ -0,0 +1,41 @@
+package com.app.technician.dto;
+
+public class TechnicianUpdateReq {
+
+    private String id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
